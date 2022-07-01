ef3fb2424b37e2f070d59e93a3a716b1cb602c23
Raphael <raphaelrodrigues93@gmail.com>
2022-06-30 21:03:02 -0300

    criando pagina no firebase

---
+++ public/src/js/config.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAruAOEWOVbpV4iQQuAkaufgfx5B2kfxOo",
+    authDomain: "controle-de-estoque-1a871.firebaseapp.com",
+    projectId: "controle-de-estoque-1a871",
+    storageBucket: "controle-de-estoque-1a871.appspot.com",
+    messagingSenderId: "843010461250",
+    appId: "1:843010461250:web:0914452e6fb62c511027db",
+    measurementId: "G-9F646XWMZT"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
